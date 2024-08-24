22c36c7c581513250a65e8e2e2bc29d6f6a2c38b
Harsh Manek <harshmanek786@gmail.com>
2024-08-24 17:50:11 +0530

    created homepage and changed the ui of login page

--- lib/initial_page.dart
+++ lib/initial_page.dart
@@ -1,1 +1,32 @@
+import 'package:flutter/material.dart';
 
+class MyHome extends StatelessWidget {
+  const MyHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Welcome to My Home Page!',
+              style: TextStyle(fontSize: 24),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Navigate to another page or perform an action
+              },
+              child: const Text('Get Started'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:vote_karo/firebase_options.dart';
 import 'package:vote_karo/login.dart';
 import 'package:vote_karo/register.dart';
+import 'package:vote_karo/initial_page.dart';
+
 // import 'package:vote_karo/login.dart';
 // import 'package:vote_karo/register.dart';
 
@@ -26,8 +28,9 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => const InitialPage(),
-        '/login':(context) => const MyLogin(),
-        '/register':(context) => const MyRegister(),
+        '/login': (context) => const MyLogin(),
+        '/register': (context) => const MyRegister(),
+        '/home':(context) => const MyHome(),
       },
       onUnknownRoute: (settings) {
         return MaterialPageRoute(builder: (context) => NotFoundPage());
@@ -36,7 +39,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class NotFoundPage extends StatelessWidget{
+class NotFoundPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -44,106 +47,111 @@ class NotFoundPage extends StatelessWidget{
   }
 }
 
-class InitialPage extends StatelessWidget{
+class InitialPage extends StatelessWidget {
   const InitialPage({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-    body: SafeArea(
-      child: Container(
-        padding: const EdgeInsets.all(16.0),
-        decoration:const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Colors.white70, Colors.lightBlueAccent],  // Start and end colors of the gradient
-            begin: Alignment.topLeft,  // Starting point of the gradient
-            end: Alignment.bottomRight,  // Ending point of the gradient
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.all(16.0),
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.white70, Colors.lightBlueAccent],
+              // Start and end colors of the gradient
+              begin: Alignment.topLeft,
+              // Starting point of the gradient
+              end: Alignment.bottomRight, // Ending point of the gradient
+            ),
           ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            const SizedBox(height: 20),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(20.0),
-              child:Stack(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.white,
-                      width: 2.0,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const SizedBox(height: 20),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20.0),
+                child: Stack(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.white,
+                          width: 2.0,
+                        ),
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      child: Image.asset(
+                        'assets/voting_homepage.png',
+                        height: 250,
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                      ),
                     ),
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  child: Image.asset(
-                    'assets/voting_homepage.png',
-                    height:250,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
+                  ],
                 ),
-                ),
-
-              ],
-            ),),
-            const Text(
-              'BallotBox',
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
               ),
-            ),
-           const Text(
-               'The Future of Voting is in Your Hands—BallotBox.',
-           style: TextStyle(
-             fontSize: 20,
-              color: Color.fromRGBO(1, 13, 30, 56),
-              fontWeight: FontWeight.bold,
-            ),
-             textAlign: TextAlign.center,
-           ),
-           const Text(
-             'Forget to stay in long queues, paperwork and methods. Vote now using BallotBox from your mobile.',
-             style: TextStyle(
-               fontSize: 16,
-               color: Color.fromRGBO(13, 30, 56, 1),
-             ),
-             textAlign: TextAlign.center,
-           ),
-           const SizedBox(height: 10),
-            Column(
-              children: [
-                ElevatedButton(
-                  onPressed: (){
-                  Navigator.pushNamed(context, '/login');
-                  },
-                  style: ElevatedButton.styleFrom(
-                    padding:const EdgeInsets.symmetric(horizontal: 105,vertical: 15),
-                    shape: RoundedRectangleBorder(
-                     borderRadius: BorderRadius.circular(25.0),
-                  ),
+              const Text(
+                'BallotBox',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
                 ),
-                   child:const Text('Login'),),
-               const SizedBox(height: 10),
-                OutlinedButton(
-                    onPressed: (){
-                      Navigator.pushNamed(context, '/register');
+              ),
+              const Text(
+                'The Future of Voting is in Your Hands—BallotBox.',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Color.fromRGBO(1, 13, 30, 56),
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const Text(
+                'Forget to stay in long queues, paperwork and methods. Vote now using BallotBox from your mobile.',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Color.fromRGBO(13, 30, 56, 1),
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 10),
+              Column(
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/login');
                     },
-                    style: OutlinedButton.styleFrom(
-                      padding:const EdgeInsets.symmetric(horizontal: 95,vertical: 15),
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 105, vertical: 15),
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(25.0),
                       ),
                     ),
-                    child:const Text('Register'),
-                ),
-              ],
-            ),
-            const SizedBox(height: 20),
-          ],
+                    child: const Text('Login'),
+                  ),
+                  const SizedBox(height: 10),
+                  OutlinedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/register');
+                    },
+                    style: OutlinedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 95, vertical: 15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                      ),
+                    ),
+                    child: const Text('Register'),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),
         ),
       ),
-    ),
     );
   }
 }
